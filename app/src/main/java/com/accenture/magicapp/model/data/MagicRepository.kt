e5cfc064346f58f6ea2d844d708aa3bc4fedad94
Victor Amaral <vic_amaral_23@hotmail.com>
2021-04-22 18:55:48 -0300

    Adiciona verificação de internet no repository

--- app/src/main/java/com/accenture/magicapp/model/data/MagicRepository.kt
+++ app/src/main/java/com/accenture/magicapp/model/data/MagicRepository.kt
@@ -1,6 +1,9 @@
 package com.accenture.magicapp.model.data
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import com.accenture.magicapp.model.data.entities.CardItemEntity
 import com.accenture.magicapp.model.data.local.CardDatabase
 import com.accenture.magicapp.model.data.pojo.CardResponse
@@ -41,5 +44,39 @@ class MagicRepository(context: Context) {
 
     fun getFavoritesCards():Observable<List<CardItemEntity>> {
         return cardDatabase.loadCards()
+    }
+
+    fun isConnectionAvailable(context: Context): Boolean {
+        var isAvailable = false
+        val connectionManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities =
+                connectionManager.activeNetwork ?: return false
+            val actNw =
+                connectionManager.getNetworkCapabilities(networkCapabilities) ?: return false
+
+            isAvailable = when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+
+            }
+        } else {
+            connectionManager.run {
+                connectionManager.activeNetworkInfo?.run {
+                    isAvailable = when (type) {
+                        ConnectivityManager.TYPE_WIFI -> true
+                        ConnectivityManager.TYPE_MOBILE -> true
+                        ConnectivityManager.TYPE_ETHERNET -> true
+                        else -> false
+
+                    }
+                }
+            }
+        }
+
+        return isAvailable
     }
 }
