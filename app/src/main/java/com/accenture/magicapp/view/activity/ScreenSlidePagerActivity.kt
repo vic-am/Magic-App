71cb2b63469354ded085b9da240a5aa73d722647
Victor Amaral <vic_amaral_23@hotmail.com>
2021-04-23 13:53:14 -0300

    Adiciona o placeholde também na ScreenSlidePagerActivity.kt

--- app/src/main/java/com/accenture/magicapp/view/activity/ScreenSlidePagerActivity.kt
+++ app/src/main/java/com/accenture/magicapp/view/activity/ScreenSlidePagerActivity.kt
@@ -1,31 +1,40 @@
 package com.accenture.magicapp.view.activity
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.widget.ViewPager2
+import coil.ImageLoader
+import coil.request.ImageRequest
 import com.accenture.magicapp.R
 import com.accenture.magicapp.model.data.pojo.CardsItem
 import com.accenture.magicapp.view.adapter.ScreenSlidePageAdapter
-import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.activity_screen_slide.*
 
 class ScreenSlidePagerActivity : FragmentActivity() {
 
     private lateinit var viewPager: ViewPager2
+    private lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen_slide)
 
         viewPager = findViewById(R.id.pager)
+        imageView = findViewById(R.id.viewPagerImageView)
         val pagerAdapter = ScreenSlidePageAdapter(this)
         viewPager.adapter = pagerAdapter
 
         setImageDetail()
     }
 
-    fun setImageDetail() {
+    private fun setImageDetail() {
         val card: CardsItem = intent.extras?.get("cards") as CardsItem
-        Picasso.get().load(card.imageUrl).into(viewPagerImageView)
+        val imageLoader = ImageLoader(applicationContext)
+        val request = ImageRequest.Builder(applicationContext)
+            .data(card.imageUrl)
+            .target(imageView)
+            .placeholder(R.drawable.nocard)
+            .build()
+        imageLoader.enqueue(request)
     }
 }
